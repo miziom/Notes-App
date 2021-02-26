dc96f4ff4e67ff6919557831f55a2b5e8448d85b
miziom <mateusz1997-11@o2.pl>
2021-02-26 20:40:54 +0100

    add unit tests for Note class

--- notes-api/src/test/java/pl/mizio/notesapi/NotesApiApplicationTests.java
+++ notes-api/src/test/java/pl/mizio/notesapi/NotesApiApplicationTests.java
@@ -1,13 +1,21 @@
 package pl.mizio.notesapi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class NotesApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void creatingNoteObject() {
+
     }
 
 }
