c7c0580a0574dcfdb1b6407022b54cb664bd188b
miziom <mateusz1997-11@o2.pl>
2021-02-26 20:56:46 +0100

    resolve dependency error

--- notes-api/src/main/java/pl/mizio/notesapi/NotesApiApplication.java
+++ notes-api/src/main/java/pl/mizio/notesapi/NotesApiApplication.java
@@ -1,13 +1,14 @@
 package pl.mizio.notesapi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 public class NotesApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NotesApiApplication.class, args);
+        run(NotesApiApplication.class, args);
     }
 
 }
